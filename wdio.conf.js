2f485a4cdf0db48a0e538af505fa98a03dec97bd
Tran Kim Chau <chau-tran@cybozu.vn>
2020-12-09 23:01:08 +0900

    first commit

--- wdio.conf.js
+++ wdio.conf.js
@@ -158,7 +158,7 @@ exports.config = {
   // See the full list at http://mochajs.org/
   mochaOpts: {
     ui: 'bdd',
-    timeout: 60000,
+    timeout: 180000,
     require: 'ts-node/register',
     compilers: [
       // optional
